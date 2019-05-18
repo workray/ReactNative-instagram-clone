026b41009abbde24dbcd0d3e61be787f26db7b06
workray <workrayz@gmail.com>
2019-05-18 03:08:56 +0200

    top navigation

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import MainScreen from './Components/MainScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppStackNavigator = createStackNavigator({
+  Main: {
+    screen: MainScreen,
   },
 });
+
+export default createAppContainer(AppStackNavigator);
--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Platform, View, Text } from 'react-native';
+import { Icon } from 'native-base';
 
 export default class App extends Component {
+    // navigationOptions
+    static navigationOptions = {
+        headerLeft: <Icon name='ios-camera' style={{ paddingLeft: 10 }}/>,
+        title: 'Instagram',
+        headerRight: <Icon name='ios-send' style={{ paddingRight: 10 }}/>,
+    }
+
     render() {
         return (
-            <View style={StyleSheet.container}>
+            <View style={styles.container}>
                 <Text>MainScreen</Text>
             </View>
         )
@@ -14,7 +22,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alginItems: 'center',
+        alignItems: 'center',
         justifyContent: 'center',
     },
 });
