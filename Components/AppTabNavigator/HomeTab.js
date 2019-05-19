b54262318f705f408f26a31eb2faeb7663c372c3
workray <workrayz@gmail.com>
2019-05-19 20:31:15 +0200

    add story header

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Container, Content, Icon } from "native-base";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { Container, Content, Icon, Thumbnail } from "native-base";
 import CardComponent from "../CardComponent";
 
 export default class HomeTab extends Component {
@@ -45,8 +45,91 @@ export default class HomeTab extends Component {
     return (
       <Container style={style.container}>
         <Content>
-          {this.state.feeds.map(feed => (
-            <CardComponent data={feed} />
+          <View style={{ height: 100 }}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingHorizontal: 7
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Stories</Text>
+
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Icon name="md-play" style={{ fontSize: 14 }} />
+                <Text style={{ fontWeight: "bold" }}> Watch All</Text>
+              </View>
+            </View>
+
+            <View style={{ flex: 3 }}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                  alignItems: "center",
+                  paddingStart: 5,
+                  paddingEnd: 5
+                }}
+              >
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={{ uri: "https://steemitimages.com/u/jacobyu/avatar" }}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={{
+                    uri: "https://steemitimages.com/u/blockchainstudio/avatar"
+                  }}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={{ uri: "https://steemitimages.com/u/gomdory/avatar" }}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={{ uri: "https://steemitimages.com/u/bbooaae/avatar" }}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={{
+                    uri: "https://steemitimages.com/u/codingman/avatar"
+                  }}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={{ uri: "https://steemitimages.com/u/bukio/avatar" }}
+                />
+              </ScrollView>
+            </View>
+          </View>
+          {this.state.feeds.map((feed, index) => (
+            <CardComponent key={"feed" + index} data={feed} />
           ))}
         </Content>
       </Container>
--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,60 +1,67 @@
-import React, { Component } from 'react';
-import { StyleSheet, Platform } from 'react-native';
-import { Icon } from 'native-base';
-import { createBottomTabNavigator, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation';
+import React, { Component } from "react";
+import { StyleSheet, Platform } from "react-native";
+import { Icon } from "native-base";
+import {
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  createAppContainer
+} from "react-navigation";
 
-import HomeTab from './AppTabNavigator/HomeTab';
-import SearchTab from './AppTabNavigator/SearchTab';
-import AddMediaTab from './AppTabNavigator/AddMediaTab';
-import LikesTab from './AppTabNavigator/LikesTab';
-import ProfileTab from './AppTabNavigator/ProfileTab';
+import HomeTab from "./AppTabNavigator/HomeTab";
+import SearchTab from "./AppTabNavigator/SearchTab";
+import AddMediaTab from "./AppTabNavigator/AddMediaTab";
+import LikesTab from "./AppTabNavigator/LikesTab";
+import ProfileTab from "./AppTabNavigator/ProfileTab";
 
-const AppTabNavigator = createMaterialTopTabNavigator({
-    HomeTab:{ screen: HomeTab },
-    Search:{ screen: SearchTab },
-    AddMedia:{ screen: AddMediaTab },
-    Likes:{ screen: LikesTab },
-    Profile:{ screen: ProfileTab }
- }, {
+const AppTabNavigator = createMaterialTopTabNavigator(
+  {
+    HomeTab: { screen: HomeTab },
+    Search: { screen: SearchTab },
+    AddMedia: { screen: AddMediaTab },
+    Likes: { screen: LikesTab },
+    Profile: { screen: ProfileTab }
+  },
+  {
     animationEnabled: true,
     swipeEnabled: true,
     tabBarPosition: "bottom",
     tabBarOptions: {
       style: {
         ...Platform.select({
-          ios:{
-            backgroundColor:'white',
+          ios: {
+            backgroundColor: "white"
           }
         })
       },
-      iconStyle: { height: 100 },
-      activeTintColor: '#000',
-      inactiveTintColor: '#d1cece',
+      iconStyle: { height: 40 },
+      activeTintColor: "#000",
+      inactiveTintColor: "#d1cece",
       upperCaseLabel: false,
       showLabel: false,
-      showIcon: true,
+      showIcon: true
     }
-});
+  }
+);
 
-const AppTabContainer = createAppContainer(AppTabNavigator)
+const AppTabContainer = createAppContainer(AppTabNavigator);
 
 export default class App extends Component {
-    // navigationOptions
-    static navigationOptions = {
-        headerLeft: <Icon name='ios-camera' style={{ paddingLeft: 10 }}/>,
-        title: 'Instagram',
-        headerRight: <Icon name='ios-send' style={{ paddingRight: 10 }}/>,
-    }
+  // navigationOptions
+  static navigationOptions = {
+    headerLeft: <Icon name="ios-camera" style={{ paddingLeft: 10 }} />,
+    title: "Instagram",
+    headerRight: <Icon name="ios-send" style={{ paddingRight: 10 }} />
+  };
 
-    render() {
-        return <AppTabContainer/>
-    }
+  render() {
+    return <AppTabContainer />;
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
